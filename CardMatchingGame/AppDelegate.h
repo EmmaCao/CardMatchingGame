d6ef2bb95eb5a76b92d63b4cd50a4ba51adf501e
曹琴 <caoqin@csike.com>
2018-10-19 13:34:27 +0800

    init

---
+++ CardMatchingGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CardMatchingGame
+//
+//  Created by xhand on 2018/10/19.
+//  Copyright © 2018年 EmmaCao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
