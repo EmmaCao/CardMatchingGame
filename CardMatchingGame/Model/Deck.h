e24d2598fb1ba1f4ec9effc8e43e1da23f471e4a
曹琴 <caoqin@csike.com>
2018-10-19 18:06:49 +0800

    add deck

---
+++ CardMatchingGame/Model/Deck.h
@@ -0,0 +1,23 @@
+//
+//  Deck.h
+//  CardMatchingGame
+//
+//  Created by xhand on 2018/10/19.
+//  Copyright © 2018年 EmmaCao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "Card.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Deck : NSObject
+
+-(void)addCard:(Card *)card atTop:(BOOL)atTop;
+
+-(Card *)drawRandomCard;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CardMatchingGame/Model/Deck.m
@@ -0,0 +1,13 @@
+//
+//  Deck.m
+//  CardMatchingGame
+//
+//  Created by xhand on 2018/10/19.
+//  Copyright © 2018年 EmmaCao. All rights reserved.
+//
+
+#import "Deck.h"
+
+@implementation Deck
+
+@end
